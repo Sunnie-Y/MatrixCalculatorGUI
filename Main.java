d95708a5e959cf523cdcd03e9d3e353761f605b5
Tomasz Grabarczyk <32244889+tomasz-grabarczyk@users.noreply.github.com>
2018-08-15 12:32:13 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package matrixCalculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MatrixCalculatorGUI matrixCalculatorGUI = new MatrixCalculatorGUI();
+        matrixCalculatorGUI.buildMatrixCalculatorGUI();
+    }
+}
